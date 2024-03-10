35d600e604fa77b94b522a22437287c176bce8c3
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-10 13:36:57 -0400

    Create global variables and a funtion to update them

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { regularVerbs } from './constants.js';
+import { regularVerbs, verbsEndings, pronouns } from './constants.js';
 
 const currentVerbEl = document.querySelector('#current-verb');
+const verifyBtn = document.querySelector('#button-verify')
+const nextBtn = document.querySelector('#button-next')
 
 /**
  * return a random number between zero and a max number
@@ -12,4 +14,44 @@ export function getRandomNum(maxNumber) {
   return Math.floor(Math.random() * maxNumber);
 }
 
-currentVerbEl.textContent = regularVerbs[getRandomNum(regularVerbs.length)];
+let currentVerb, verbWithoutEnding, infinitiveVerbEnding, arrayOfVerbEndinds;
+let selectedIndex;
+let pronoun, correntVerbEnding, conjugation;
+
+
+function updateVerb() {
+  // pick a random verb from the array
+  currentVerb = regularVerbs[getRandomNum(regularVerbs.length)]
+
+  // get the verb without the ending ar | er | ir
+  verbWithoutEnding = currentVerb.slice(0, -2);
+
+  // get the verb the ending ar | er | ir
+  infinitiveVerbEnding = currentVerb.slice(-2);
+  
+  // get an array verb endings from the verbEndings object
+  // based on the ending of the current verb
+  arrayOfVerbEndinds = verbsEndings[infinitiveVerbEnding];
+  
+  // get a random number that will be used to pick the pronoun
+  // and the corresponding conjugation ending of that verb
+  selectedIndex = getRandomNum(pronouns.length);
+
+  pronoun = pronouns[selectedIndex];
+  correntVerbEnding = arrayOfVerbEndinds[selectedIndex];
+  conjugation = verbWithoutEnding + correntVerbEnding;
+}
+
+updateVerb()
+
+
+currentVerbEl.textContent = currentVerb;
+
+verifyBtn.addEventListener('click', () => {
+  console.log('verify button clicked')
+})
+
+nextBtn.addEventListener('click', () => {
+  updateVerb()
+  currentVerbEl.textContent = currentVerb;
+})
